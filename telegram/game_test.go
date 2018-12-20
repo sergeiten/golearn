18b8b2d84a7d52e09e1240330a0d137f4fb764fe
Sergei Ten <sergei.ten@gmail.com>
2018-12-20 19:46:20 +0900

    Added more tests.

--- telegram/game_test.go
+++ telegram/game_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/sergeiten/golearn"
 	"github.com/sergeiten/golearn/mocks"
@@ -503,4 +504,399 @@ func TestAnswer(t *testing.T) {
 			dbService.AssertExpectations(t)
 		})
 	}
+}
+
+func TestStartWithTypingMode(t *testing.T) {
+	sampleError := errors.New("sample error")
+
+	update := golearn.Update{
+		ChatID:   "177374215",
+		UserID:   "177374215",
+		Username: "sergeiten",
+		Name:     "Sergei",
+		Message:  "command",
+	}
+
+	testCases := map[string]struct {
+		Message       string
+		Markup        ReplyMarkup
+		Error         error
+		Question      golearn.Row
+		RandomError   error
+		SetStateError error
+	}{
+		"with random question error": {
+			Message:       "",
+			Markup:        ReplyMarkup{},
+			Error:         sampleError,
+			Question:      golearn.Row{},
+			RandomError:   sampleError,
+			SetStateError: nil,
+		},
+		"with set state error": {
+			Message:       "",
+			Markup:        ReplyMarkup{},
+			Error:         sampleError,
+			Question:      golearn.Row{},
+			RandomError:   nil,
+			SetStateError: sampleError,
+		},
+		"no errors": {
+			Message: "question translate",
+			Markup: ReplyMarkup{
+				[][]string{
+					{
+						lang["next_word"],
+						lang["show_answer"],
+						lang["main_menu"],
+					},
+				},
+				true,
+			},
+			Error: nil,
+			Question: golearn.Row{
+				Word:      "question word",
+				Translate: "question translate",
+			},
+			RandomError:   nil,
+			SetStateError: nil,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			dbService := &mocks.DBService{}
+			httpService := &mocks.HttpService{}
+
+			handler = New(HandlerConfig{
+				DBService:       dbService,
+				HTTPService:     httpService,
+				Lang:            lang,
+				DefaultLanguage: "ru",
+				Token:           botToken,
+				ColsCount:       2,
+			})
+
+			dbService.On("RandomQuestion").Return(tc.Question, tc.RandomError)
+			if tc.RandomError == nil {
+				dbService.On("SetState", golearn.State{
+					UserKey:   update.UserID,
+					Question:  tc.Question,
+					Answers:   []golearn.Row{},
+					Timestamp: time.Now().Unix(),
+				}).Return(tc.SetStateError)
+			}
+
+			message, markup, err := handler.startWithTypingMode(&update)
+
+			assert.Equal(t, tc.Message, message)
+			assert.Equal(t, tc.Markup, markup)
+			assert.Equal(t, tc.Error, err)
+
+			dbService.AssertExpectations(t)
+		})
+	}
+}
+
+func TestStartWithPickingModeWithRandomQuestionError(t *testing.T) {
+	sampleError := errors.New("sample error")
+
+	update := golearn.Update{
+		ChatID:   "177374215",
+		UserID:   "177374215",
+		Username: "sergeiten",
+		Name:     "Sergei",
+		Message:  "command",
+	}
+
+	expectedMessage := ""
+	expectedMarkup := ReplyMarkup{}
+	expectedError := sampleError
+
+	dbService := &mocks.DBService{}
+	httpService := &mocks.HttpService{}
+
+	handler = New(HandlerConfig{
+		DBService:       dbService,
+		HTTPService:     httpService,
+		Lang:            lang,
+		DefaultLanguage: "ru",
+		Token:           botToken,
+		ColsCount:       2,
+	})
+
+	dbService.On("RandomQuestion").Return(golearn.Row{}, sampleError)
+
+	message, markup, err := handler.startWithPickingMode(&update)
+
+	assert.Equal(t, expectedMessage, message)
+	assert.Equal(t, expectedMarkup, markup)
+	assert.Equal(t, expectedError, err)
+
+	dbService.AssertExpectations(t)
+}
+
+func TestStartWithPickingModeWithRandomAnswersError(t *testing.T) {
+	sampleError := errors.New("sample error")
+
+	update := golearn.Update{
+		ChatID:   "177374215",
+		UserID:   "177374215",
+		Username: "sergeiten",
+		Name:     "Sergei",
+		Message:  "command",
+	}
+
+	question := golearn.Row{
+		Word:      "question word",
+		Translate: "question translate",
+	}
+
+	expectedMessage := ""
+	expectedMarkup := ReplyMarkup{}
+	expectedError := sampleError
+
+	dbService := &mocks.DBService{}
+	httpService := &mocks.HttpService{}
+
+	handler = New(HandlerConfig{
+		DBService:       dbService,
+		HTTPService:     httpService,
+		Lang:            lang,
+		DefaultLanguage: "ru",
+		Token:           botToken,
+		ColsCount:       2,
+	})
+
+	dbService.On("RandomQuestion").Return(question, nil)
+	dbService.On("RandomAnswers", question, 4).Return([]golearn.Row{}, sampleError)
+
+	message, markup, err := handler.startWithPickingMode(&update)
+
+	assert.Equal(t, expectedMessage, message)
+	assert.Equal(t, expectedMarkup, markup)
+	assert.Equal(t, expectedError, err)
+
+	dbService.AssertExpectations(t)
+}
+
+func TestStartWithPickingModeWithEmptyAnswersError(t *testing.T) {
+	update := golearn.Update{
+		ChatID:   "177374215",
+		UserID:   "177374215",
+		Username: "sergeiten",
+		Name:     "Sergei",
+		Message:  "command",
+	}
+
+	question := golearn.Row{
+		Word:      "question word",
+		Translate: "question translate",
+	}
+
+	answers := []golearn.Row{}
+
+	expectedMessage := lang["no_words"]
+	expectedMarkup := ReplyMarkup{}
+
+	dbService := &mocks.DBService{}
+	httpService := &mocks.HttpService{}
+
+	handler = New(HandlerConfig{
+		DBService:       dbService,
+		HTTPService:     httpService,
+		Lang:            lang,
+		DefaultLanguage: "ru",
+		Token:           botToken,
+		ColsCount:       2,
+	})
+
+	dbService.On("RandomQuestion").Return(question, nil)
+	dbService.On("RandomAnswers", question, 4).Return(answers, nil)
+
+	message, markup, err := handler.startWithPickingMode(&update)
+
+	assert.Equal(t, expectedMessage, message)
+	assert.Equal(t, expectedMarkup, markup)
+	assert.Equal(t, nil, err)
+
+	dbService.AssertExpectations(t)
+}
+
+func TestStartWithPickingModeWithSetStateError(t *testing.T) {
+	sampleError := errors.New("sample error")
+
+	update := golearn.Update{
+		ChatID:   "177374215",
+		UserID:   "177374215",
+		Username: "sergeiten",
+		Name:     "Sergei",
+		Message:  "command",
+	}
+
+	question := golearn.Row{
+		Word:      "question word",
+		Translate: "question translate",
+	}
+
+	answers := []golearn.Row{
+		{
+			Word:      "answer word 1",
+			Translate: "answer translate 1",
+		},
+		{
+			Word:      "answer word 2",
+			Translate: "answer translate 2",
+		},
+		{
+			Word:      "answer word 3",
+			Translate: "answer translate 3",
+		},
+		{
+			Word:      "answer word 4",
+			Translate: "answer translate 4",
+		},
+	}
+
+	expectedMessage := ""
+	expectedMarkup := ReplyMarkup{}
+
+	dbService := &mocks.DBService{}
+	httpService := &mocks.HttpService{}
+
+	handler = New(HandlerConfig{
+		DBService:       dbService,
+		HTTPService:     httpService,
+		Lang:            lang,
+		DefaultLanguage: "ru",
+		Token:           botToken,
+		ColsCount:       2,
+	})
+
+	dbService.On("RandomQuestion").Return(question, nil)
+	dbService.On("RandomAnswers", question, 4).Return(answers, nil)
+	dbService.On("SetState", golearn.State{
+		UserKey:   update.UserID,
+		Question:  question,
+		Answers:   answers,
+		Timestamp: time.Now().Unix(),
+	}).Return(sampleError)
+
+	message, markup, err := handler.startWithPickingMode(&update)
+
+	assert.Equal(t, expectedMessage, message)
+	assert.Equal(t, expectedMarkup, markup)
+	assert.Equal(t, sampleError, err)
+
+	dbService.AssertExpectations(t)
+}
+
+func TestStartWithPickingMode(t *testing.T) {
+	update := golearn.Update{
+		ChatID:   "177374215",
+		UserID:   "177374215",
+		Username: "sergeiten",
+		Name:     "Sergei",
+		Message:  "command",
+	}
+
+	question := golearn.Row{
+		Word:      "question word",
+		Translate: "question translate",
+	}
+
+	answers := []golearn.Row{
+		{
+			Word:      "answer word",
+			Translate: "answer translate",
+		},
+		{
+			Word:      "answer word",
+			Translate: "answer translate",
+		},
+		{
+			Word:      "answer word",
+			Translate: "answer translate",
+		},
+		{
+			Word:      "answer word",
+			Translate: "answer translate",
+		},
+	}
+
+	expectedMessage := "question word"
+	expectedMarkup := ReplyMarkup{
+		Keyboard: [][]string{
+			{
+				"answer translate",
+				"answer translate",
+			},
+			{
+				"answer translate",
+				"answer translate",
+			},
+			{
+				lang["main_menu"],
+			},
+		},
+		ResizeKeyboard: true,
+	}
+
+	dbService := &mocks.DBService{}
+	httpService := &mocks.HttpService{}
+
+	handler = New(HandlerConfig{
+		DBService:       dbService,
+		HTTPService:     httpService,
+		Lang:            lang,
+		DefaultLanguage: "ru",
+		Token:           botToken,
+		ColsCount:       2,
+	})
+
+	dbService.On("RandomQuestion").Return(question, nil)
+	dbService.On("RandomAnswers", question, 4).Return(answers, nil)
+	dbService.On("SetState", golearn.State{
+		UserKey:   update.UserID,
+		Question:  question,
+		Answers:   answers,
+		Timestamp: time.Now().Unix(),
+	}).Return(nil)
+
+	message, markup, err := handler.startWithPickingMode(&update)
+
+	assert.Equal(t, expectedMessage, message)
+	assert.Equal(t, expectedMarkup, markup)
+	assert.Equal(t, nil, err)
+
+	dbService.AssertExpectations(t)
+}
+
+func TestStart(t *testing.T) {
+	handler = New(HandlerConfig{
+		DBService:       nil,
+		HTTPService:     nil,
+		Lang:            lang,
+		DefaultLanguage: "ru",
+		Token:           botToken,
+		ColsCount:       2,
+	})
+
+	handler.user.Mode = "set no existed mode"
+
+	update := golearn.Update{
+		ChatID:   "177374215",
+		UserID:   "177374215",
+		Username: "sergeiten",
+		Name:     "Sergei",
+		Message:  "command",
+	}
+
+	sampleError := fmt.Errorf("failed to start, undefined mode for user: %v", handler.user)
+
+	message, markup, err := handler.start(&update)
+
+	assert.Equal(t, "", message)
+	assert.Equal(t, ReplyMarkup{}, markup)
+	assert.Equal(t, sampleError, err)
 }
