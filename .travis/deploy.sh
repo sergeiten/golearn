8b4706f9464177f60a628ec300232835628aaad6
Sergei Ten <sergei.ten@gmail.com>
2019-02-19 12:09:45 +0900

    CI/CD

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,8 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/id_rsa
 ssh-add .travis/id_rsa
 
+ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -H $DEPLOY_URL 2>&1 | tee -a $HOME/.ssh/known_hosts
+
 git config --global push.default matching
 git remote add deploy ssh://$DEPLOY_USER@$DEPLOY_URL:$DEPLOY_DIR
 git push deploy master
